5e984995f6e4b001bcdc8725d60003f29a81a8ce
atteas <atte.aaltosetala@gmail.com>
2025-05-03 14:14:18 +0300

    Showing FPS

--- engine/io/Window.java
+++ engine/io/Window.java
@@ -7,6 +7,8 @@ public class Window {
     private int width,  height;
     private String title;
     private long window;
+    public int frames;
+    public long time;
 
     public Window(int width, int height, String title) {
         this.width = width;
@@ -33,11 +35,21 @@ public class Window {
 
         GLFW.glfwShowWindow(window);
 
-        GLFW.glfwSwapInterval(1); //rajotus 60fps
+        GLFW.glfwSwapInterval(1);
+
+        time = System.currentTimeMillis();
     }
 
     public void update() {
         GLFW.glfwPollEvents();
+
+        //check frames (print every second and reset the counter)
+        frames++;
+        if (System.currentTimeMillis() > time + 1000){
+            GLFW.glfwSetWindowTitle(window, title + " | FPS: " + frames);
+            time = System.currentTimeMillis();
+            frames = 0;
+        }
     }
 
     public void swapBuffers() {
