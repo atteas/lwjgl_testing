e2df23403cde37eabb38f5642e9133359bb04fe1
atteas <atte.aaltosetala@gmail.com>
2025-05-08 13:35:46 +0300

    Last version before next tutorial. Texture doesn't take the whole background as it's supposed to. Let's see if the next tutorial fixes this.

--- resources/shaders/mainVertex.glsl
+++ resources/shaders/mainVertex.glsl
@@ -1,4 +1,4 @@
-#version 460 core
+#version 330 core
 
 in vec3 position;
 in vec3 color;
@@ -10,7 +10,7 @@ out vec2 passTextureCoord;
 uniform mat4 model;
 
 void main(){
-    gl_Position = vec4(position, 1.0) * model;
+    gl_Position = model * vec4(position, 1.0);
     passColor = color;
     passTextureCoord = textureCoord;
 }
